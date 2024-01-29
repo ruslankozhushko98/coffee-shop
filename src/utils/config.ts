68943adccbf7f0d5f3f2633f7e51527441f3cd92
ruslankozhushko98 <ruslankozhushko98@gmail.com>
2024-01-29 18:37:16 -0500

    feat: setup env vars;

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,10 +1,9 @@
-import Config from 'react-native-config';
+import 'react-native-url-polyfill/auto';
+
 import { QueryClient } from '@tanstack/react-query';
 import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_URL, API_KEY } from '@env';
 
 export const queryClient = new QueryClient();
 
-// export const supabase = createClient(
-//   String(Config.SUPABASE_URL),
-//   String(Config.API_KEY),
-// );
+export const supabase = createClient(String(SUPABASE_URL), String(API_KEY));
