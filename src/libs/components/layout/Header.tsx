9adff465aa1461bb2d675874172b9f87c04b32ac
ruslankozhushko98 <ruslankozhushko98@gmail.com>
2024-03-30 11:28:51 -0400

    feat: minor header update;

--- src/libs/components/layout/Header.tsx
+++ src/libs/components/layout/Header.tsx
@@ -1,18 +1,13 @@
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useNavigation } from '@react-navigation/native';
 import { BottomTabHeaderProps } from '@react-navigation/bottom-tabs';
-import { Box, Button, Text } from 'native-base';
+import { Box, Text } from 'native-base';
 
 import { useGlobalContext } from 'contexts/globalContext';
-import { Screens } from 'libs/utils/constants';
 
 export const Header: FC<BottomTabHeaderProps> = () => {
   const { t } = useTranslation();
-  const { navigate } = useNavigation();
   const { user } = useGlobalContext();
-
-  const handleGoToSignIn = (): void => navigate(Screens.SIGN_IN_SCREEN);
 
   return (
     <Box
@@ -26,19 +21,6 @@ export const Header: FC<BottomTabHeaderProps> = () => {
       <Text fontWeight="bold" fontSize="xl" color="tertiary.600">
         {t('header:title', { name: user?.firstName || t('header:guest') })}
       </Text>
-
-      {!user && (
-        <Button variant="ghost" onPress={handleGoToSignIn}>
-          <Text
-            fontWeight="bold"
-            fontSize="lg"
-            color="blue.600"
-            textDecorationLine="underline"
-          >
-            {t('links:signIn')}
-          </Text>
-        </Button>
-      )}
     </Box>
   );
 };
